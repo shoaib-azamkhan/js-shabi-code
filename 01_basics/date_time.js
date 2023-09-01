8402da30e80babe57afe53f0f4830a4c04d0072a
Shoaib_Azam8 <69983752+shoaib-azamkhan@users.noreply.github.com>
2023-09-01 20:39:44 +0000

    dates and time

---
+++ 01_basics/date_time.js
@@ -0,0 +1,8 @@
+//Dates----------:
+let myDate= new Date();
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+console.log(myDate.toLocaleDateString());
+console.log(myDate.getDay());
+console.log(myDate.getMonth());
